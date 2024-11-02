306060acdceb2bb4437bd89dea1599080864d2af
huixin35 <115418787+huixin35@users.noreply.github.com>
2024-11-02 11:45:41 +0800

    Add files via upload

---
+++ huixinzhu-asedemo-627f3fda384d/ASEdemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ASEdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Cruel World!");
+            Console.WriteLine("new line");
+        }
+    }
+}
